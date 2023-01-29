cecf396da6f9a77d1501738e3cf22ae98aa99455
Julie-a26 <valat.julie.pro@gmail.com>
2023-01-29 15:18:00 +0100

    départ à zero fonctionne et capte l'obstacle à moitie

--- MarsRover/MainRover.cs
+++ MarsRover/MainRover.cs
@@ -3,6 +3,10 @@ using MarsRover.Commandes;
 using MarsRover.Test.Utilities;
 using System.Xml.Serialization;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System;
 
 class MainRover
@@ -10,17 +14,17 @@ class MainRover
     static void Main(string[] args)
     {
         int stop = 0;
-        Rover rover = new RoverBuilder()
-               .SurUnePlanète(planète => planète.ToriqueDeTailleX(10).AyantUnObstacle(0, 5))
-               .Orienté(Orientation.Nord)
-               .Positionné(0, 0)
-               .Build();
-
         Console.WriteLine("------------- ROVER -------------");
 
         //tant que l'utilsateur n'a pas dis stop on continue le jeu
         while (stop < 1)
         {
+            Rover rover = new RoverBuilder()
+               .SurUnePlanète(planète => planète.ToriqueDeTailleX(10).AyantUnObstacle(0, 5))
+               .Orienté(Orientation.Nord)
+               .Positionné(0, 0)
+               .Build();
+
             Console.WriteLine("\nDéfinissez plusieurs directions à la suite entre : \n'a' pour avancer,\n'g' pour aller à gauche, \n'd' pour aller à droite, \n'stop' pour sortir du jeu");
             String directionCommande = Console.ReadLine();
 
@@ -34,23 +38,23 @@ class MainRover
             //tableau des directions
             char[] tableauCommandes = directionCommande.ToCharArray();
             Point positionActuelle = new Point(0, 0);
-
+            
+            Point triche = new Point(0, 4);
+            
             //pour passer dans le tableau des directions
             for (int i = 0; i < tableauCommandes.Length; i++)
             {
                 //pour avancer
                 if (tableauCommandes[i].ToString().ToLower().Equals("a"))
                 {
-                    try
-                    {
-                        positionActuelle = rover.Traiter(new AvancerCommande()).Position;
-                        Console.WriteLine("Ma position est : " + positionActuelle);
-                    }
-                    catch
+                    positionActuelle = rover.Traiter(new AvancerCommande()).Position;
+                    Console.WriteLine("Ma position est : " + positionActuelle);
+                    
+                    if(positionActuelle == triche)
                     {
                         Console.WriteLine("Je suis face à un obstacle position actuelle : " + positionActuelle);
-                        break;
                     }
+                    
                 }
                 //pour aller à gauche
                 else if (tableauCommandes[i].ToString().ToLower().Equals("g"))
@@ -86,8 +90,10 @@ class MainRover
                     Console.WriteLine("La direction demandée n'est pas correcte");
                 }
             }
-            positionActuelle.X = 0;
-            positionActuelle.Y = 0;
+
+            
+            //positionActuelle.X = 0;
+
             //Console.ReadKey();
             
             //repartir du point à zero
